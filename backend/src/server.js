cb87e896eda9022461b399ab615d0f30b0d746c8
Azafa Dev <akmal.dz.f@gmail.com>
2026-02-21 10:09:50 +0700

    update the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
-import productRouter from './routes/product.route.js';
-import { sql } from './configs/sql.js';
-import { aj } from './libs/arcjet.js';
+import productRouter from "./routes/product.route.js";
+import { sql } from "./configs/sql.js";
+import { aj } from "./libs/arcjet.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
-app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
-app.use(morgan('dev')); // log the HTTP requests
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+app.use(helmet({
+  contentSecurityPolicy: false
+})); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the HTTP requests
 app.use(express.json());
 
 async function initDB() {
@@ -31,23 +37,23 @@ async function initDB() {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`;
 
-    console.log('Connected to the database');
+    console.log("Connected to the database");
   } catch (error) {
-    console.log('error connecting to the database', error);
+    console.log("error connecting to the database", error);
   }
 }
 
 app.use(async (req, res, next) => {
   try {
     const decision = await aj.protect(req, { requested: 1 }); // protect the request
-    console.log('arcjet decision', decision);
+    console.log("arcjet decision", decision);
     if (decision.isDenied()) {
       if (decision.reason.isRateLimit()) {
-        return res.status(429).json({ message: 'Too many requests' });
+        return res.status(429).json({ message: "Too many requests" });
       } else if (decision.reason.isBot()) {
-        return res.status(403).json({ message: 'Bot access denied' });
+        return res.status(403).json({ message: "Bot access denied" });
       } else {
-        return res.status(403).json({ message: 'Access denied' });
+        return res.status(403).json({ message: "Access denied" });
       }
     }
 
@@ -56,16 +62,23 @@ app.use(async (req, res, next) => {
         (result) => result.reason.isBot() && result.reason.spoofed,
       )
     ) {
-      return res.status(403).json({ message: 'Spoofed bot detected' });
+      return res.status(403).json({ message: "Spoofed bot detected" });
     }
     next();
   } catch (error) {
-    console.log('error arcjet', error);
+    console.log("error arcjet", error);
     next(error);
   }
 });
 
-app.use('/api/products', productRouter);
+app.use("/api/products", productRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 initDB().then(() => {
   app.listen(PORT, () => {
